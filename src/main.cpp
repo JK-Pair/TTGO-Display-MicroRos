00b61f7bc12f1d4fc747c4dfae123a4a196b4277
JK-Pair <thipawan2112@gmail.com>
2024-06-13 22:16:43 +0700

    Done1Step

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,35 @@
+// this code is example of publisher and subscriber
+// https://github.com/micro-ROS/micro_ros_arduino/blob/galactic/examples/micro-ros_subscriber/micro-ros_subscriber.ino
+// https://github.com/micro-ROS/micro_ros_arduino/blob/galactic/examples/micro-ros_publisher/micro-ros_publisher.ino
+// run micro_ros_agent before connecting arduino
+// docker run -it --rm -v /dev:/dev --privileged --net=host microros/micro-ros-agent:galactic serial --dev /dev/cu.usbmodem1422201 -v6
+
 #include <Arduino.h>
 #include <TFT_eSPI.h>
 
-#define MC_DATUM 4
+#include <micro_ros_arduino.h>
+
+#include <stdio.h>
+#include <rcl/rcl.h>
+#include <rcl/error_handling.h>
+#include <rclc/rclc.h>
+#include <rclc/executor.h>
+
+#include <std_msgs/msg/int8.h>
+
+rcl_node_t node;
+rclc_support_t support;
+rcl_allocator_t allocator;
+
+// subscriber
+rcl_subscription_t sub_state;
+std_msgs__msg__Int8 state_msg;
+rclc_executor_t executor_sub;
 
 TFT_eSPI tft = TFT_eSPI();
+#define MC_DATUM 4
+String state_str = "S:";
+String state_int = "0";
 
 String force_str = "F:";
 String force_int = "50 N";
@@ -11,96 +37,93 @@ String force_int = "50 N";
 String duty_str = "D:";
 String duty_int = "50 %";
 
-String state_str = "S:";
-String state_int = "0";
-
 String mode_str = "Manual";
 
+#define RCCHECK(fn)              \
+  {                              \
+    rcl_ret_t temp_rc = fn;      \
+    if ((temp_rc != RCL_RET_OK)) \
+    {                            \
+      error_loop();              \
+    }                            \
+  }
+#define RCSOFTCHECK(fn)          \
+  {                              \
+    rcl_ret_t temp_rc = fn;      \
+    if ((temp_rc != RCL_RET_OK)) \
+    {                            \
+    }                            \
+  }
 
-struct Button
+/**
+ * @brief loop to indicate error with blinking LED
+ *
+ */
+void error_loop()
 {
-  const uint8_t PIN;
-  bool pressed;
-};
-
-
-Button top_button = {35, false};
-Button below_button = {0, false};
-
-
-int state_machine = 0;
-
-
-void IRAM_ATTR IO_INT_ISR()
-{
-  // Toggle The state machine
-  below_button.pressed = false;
-  top_button.pressed = true;
+  while (1)
+  {
+    delay(100);
+  }
 }
 
-void IRAM_ATTR IO_INT_ISR2()
+/**
+ * @brief subscription callback executed at receiving a message
+ *
+ * @param msgin
+ */
+void sub_state_callback(const void *msgin)
 {
-  // Toggle The state machine
-  top_button.pressed = false;
-  below_button.pressed = true;
+  const std_msgs__msg__Int8 *state_msg = (const std_msgs__msg__Int8 *)msgin;
+  // (condition) ? (true exec):(false exec)
 }
 
 void setup()
-{
-  pinMode(below_button.PIN, INPUT_PULLUP);
-  pinMode(top_button.PIN, INPUT_PULLUP);
-  attachInterrupt(top_button.PIN, IO_INT_ISR, RISING);
-  attachInterrupt(below_button.PIN, IO_INT_ISR2, FALLING);
-
-
+{  
   tft.init();
   tft.setRotation(1);
+  delay(2000);
+  set_microros_transports();
+  delay(2000);
+
+  allocator = rcl_get_default_allocator();
+
+  // create init_options
+  RCCHECK(rclc_support_init(&support, 0, NULL, &allocator));
+
+  // // create node
+  RCCHECK(rclc_node_init_default(&node, "display_node", "", &support));
+
+  //create subscriber
+  RCCHECK(rclc_subscription_init_default(
+      &sub_state,
+      &node,
+      ROSIDL_GET_MSG_TYPE_SUPPORT(std_msgs, msg, Int8),
+      "current_state"));
+
+  RCCHECK(rclc_executor_init(&executor_sub, &support.context, 1, &allocator));
+  RCCHECK(rclc_executor_add_subscription(&executor_sub, &sub_state, &state_msg, &sub_state_callback, ON_NEW_DATA));
+
+
   tft.fillScreen(TFT_RED);
   delay(50);
   tft.fillScreen(TFT_GREEN);
   delay(50);
   tft.fillScreen(TFT_BLUE);
   delay(50);
-
   tft.setTextSize(6);
-  
-  // tft.drawString(force_str+force_int, 5, 10);
-  // tft.drawString(duty_str+duty_int, 5, 50);
-  // tft.drawString(state_str+state_int, 5, 95);
-
-
-  // tft.setTextDatum(MC_DATUM);
 
 }
 
 void loop()
 {
-  if (top_button.pressed)
-  {
-    force_int = "15 N";
-    duty_int = "75 %";
-    state_int = "1";
-    mode_str = "Manual";
-    
-  }
+  delay(100);
+  RCCHECK(rclc_executor_spin_some(&executor_sub, RCL_MS_TO_NS(100)));
 
-  else if (below_button.pressed)
-  {
-    force_int = "50 N";
-    duty_int = "26 %";
-    state_int = "2";
-    mode_str = "Auto  ";
-  }
-
-  tft.setTextSize(6);
-  tft.drawString(state_str+state_int, 5, 0);
+  tft.drawString(state_str+String(state_msg.data), 5, 0);
   tft.setTextSize(3);
   tft.drawString(mode_str, 125, 15);
   tft.setTextSize(6);
   tft.drawString(force_str+force_int, 5, 48);
   tft.drawString(duty_str+duty_int, 5, 95);
-
-
-
-
 }
