2006386f33fa37977b78e053a17be67163f7ff93
JK-Pair <thipawan2112@gmail.com>
2024-06-26 21:08:41 +0700

    update1

--- src/main.cpp
+++ src/main.cpp
@@ -16,26 +16,35 @@
 #include <rclc/executor.h>
 
 #include <std_msgs/msg/int8.h>
+#include <std_msgs/msg/int16.h>
+#include <std_msgs/msg/u_int8.h>
+
+
 
 rcl_node_t node;
 rclc_support_t support;
 rcl_allocator_t allocator;
+rclc_executor_t executor_sub;
 
 // subscriber
 rcl_subscription_t sub_state;
 std_msgs__msg__Int8 state_msg;
-rclc_executor_t executor_sub;
+
+rcl_subscription_t sub_force;
+std_msgs__msg__Int16 force_msg;
+
+rcl_subscription_t sub_duty;
+std_msgs__msg__UInt8 duty_msg;
 
 TFT_eSPI tft = TFT_eSPI();
 #define MC_DATUM 4
 String state_str = "S:";
-String state_int = "0";
 
 String force_str = "F:";
-String force_int = "50 N";
+String force_unit = "N";
 
 String duty_str = "D:";
-String duty_int = "50 %";
+String duty_unit = "%";
 
 String mode_str = "Manual";
 
@@ -75,9 +84,23 @@ void error_loop()
 void sub_state_callback(const void *msgin)
 {
   const std_msgs__msg__Int8 *state_msg = (const std_msgs__msg__Int8 *)msgin;
+  tft.fillScreen(TFT_BLUE);
   // (condition) ? (true exec):(false exec)
 }
 
+void sub_force_callback(const void *msgin)
+{
+  const std_msgs__msg__Int16 *force_msg = (const std_msgs__msg__Int16 *)msgin;
+  tft.fillScreen(TFT_BLUE);
+  // (condition) ? (true exec):(false exec)
+}
+
+void sub_duty_callback(const void *msgin)
+{
+  const std_msgs__msg__UInt8 *duty_msg = (const std_msgs__msg__UInt8 *)msgin;
+  tft.fillScreen(TFT_BLUE);
+  // (condition) ? (true exec):(false exec)
+}
 void setup()
 {  
   tft.init();
@@ -101,15 +124,32 @@ void setup()
       ROSIDL_GET_MSG_TYPE_SUPPORT(std_msgs, msg, Int8),
       "current_state"));
 
-  RCCHECK(rclc_executor_init(&executor_sub, &support.context, 1, &allocator));
+  RCCHECK(rclc_subscription_init_default(
+      &sub_force,
+      &node,
+      ROSIDL_GET_MSG_TYPE_SUPPORT(std_msgs, msg, Int16),
+      "force_target"));
+      
+  RCCHECK(rclc_subscription_init_default(
+      &sub_duty,
+      &node,
+      ROSIDL_GET_MSG_TYPE_SUPPORT(std_msgs, msg, UInt8),
+      "dc_driver_topic"));
+
+
+
+  RCCHECK(rclc_executor_init(&executor_sub, &support.context, 3, &allocator));
   RCCHECK(rclc_executor_add_subscription(&executor_sub, &sub_state, &state_msg, &sub_state_callback, ON_NEW_DATA));
+  RCCHECK(rclc_executor_add_subscription(&executor_sub, &sub_force, &force_msg, &sub_force_callback, ON_NEW_DATA));
+  RCCHECK(rclc_executor_add_subscription(&executor_sub, &sub_duty, &duty_msg, &sub_duty_callback, ON_NEW_DATA));
 
 
-  tft.fillScreen(TFT_RED);
-  delay(50);
-  tft.fillScreen(TFT_GREEN);
-  delay(50);
-  tft.fillScreen(TFT_BLUE);
+
+  // tft.fillScreen(TFT_RED);
+  // delay(50);
+  // tft.fillScreen(TFT_GREEN);
+  // delay(50);
+  // tft.fillScreen(TFT_BLUE);
   delay(50);
   tft.setTextSize(6);
 
@@ -124,6 +164,6 @@ void loop()
   tft.setTextSize(3);
   tft.drawString(mode_str, 125, 15);
   tft.setTextSize(6);
-  tft.drawString(force_str+force_int, 5, 48);
-  tft.drawString(duty_str+duty_int, 5, 95);
+  tft.drawString(force_str+String(force_msg.data)+force_unit, 5, 48);
+  tft.drawString(duty_str+String(duty_msg.data)+duty_unit, 5, 95);
 }
